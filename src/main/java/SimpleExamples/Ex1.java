110543265fe0dfe3b6899f2d9008f2b9d765d75f
alex <bogatyrenko@mail.ua>
2017-01-30 22:48:03 +0200

    java rush ex

---
+++ src/main/java/SimpleExamples/Ex1.java
@@ -0,0 +1,13 @@
+package SimpleExamples;
+
+/**
+ * Created by Alex on 29.10.2016.
+ */
+public class Ex1 {
+    public static void main(String[] args) {
+        int res;
+        int a,b,c;
+
+
+    }
+}
